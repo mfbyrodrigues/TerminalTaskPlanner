54926b5b3a6f00c6801755f9a420fb6c3b557c51
M. Fernanda <n.nandcchi@gmail.com>
2025-05-08 15:53:21 -0300

    Add files via upload

--- code/taskplanner.py
+++ code/taskplanner.py
@@ -4,38 +4,48 @@ tarefas = []            # Lista principal de tarefas, vazia (para ser preechida)
 historico = []          # Pilha para desfazer tarefas
 fila_execucao = []      # Fila para executar tarefas
 
+"""
+    tarefa → tarefa sem especificação.
+    tarefa_ completa → tarefa especificada: tarefa, prioridade e data.
+"""
+
 def adicionar_tarefa(): # Função para adicionar tarefas
-    descricao = input ("Digite a tarefa: ") # Tarefa digitada pelo usuário
+    # Lê a tarefa digitada pelo usuário
+    descricao = input ("Digite a tarefa: ")
     print()
 
+    # Exibe as opções e lê a prioridade escolhida pelo usuário
     print ("Escolha a prioridade abaixo ↴ \n")
     print ("🟢 » Baixa")
     print ("🟡 » Média")
     print ("🔴 » Alta \n")
     prioridade = input (": ")
 
+    # Exibe a mensagem e lê a data digitada pelo usuário
     print()
     print ("Digite a data abaixo ↴ \n")
     print ("⚠️   Siga o formato (dd/mm/aaaa). \n")
     data = input (": ")
+    print()
 
-    tarefa_completa = { # Meu dicionário para organizar......... (Chave e valor)
-        'tarefa': descricao,
-        'prioridade': prioridade,
-        'data': data
+    tarefa_completa = { # Meu dicionário para organizar os dados da tarefa com suas respectivas chaves e valores
+        'tarefa': descricao, # chave 'tarefa' armazena a descrição da tarefa
+        'prioridade': prioridade, # chave 'prioridade' armazena a prioridade escolhida (🟢, 🟡, 🔴)
+        'data': data # chave 'data' armazena a data informada pelo usuário (no formato dd/mm/aaaa)
     }
 
-    print (f"Prioridade: {tarefa_completa['prioridade']} | Data: {tarefa_completa['data']}\n")
-
+    # Adiciona a tarefa na lista principal, no histórico e na fila de execução
     tarefas.append(tarefa_completa)
     historico.append(tarefa_completa)
     fila_execucao.append(tarefa_completa)
+    # Ou seja: "append" significa adicionar um item ao final de uma lista
 
-    print (f"Tarefa '{tarefa_completa['tarefa']}' adicionada com sucesso! \n") # Resposta ao usuário
+    print (f"Tarefa '{tarefa_completa['tarefa']}' adicionada com sucesso!") # Resposta ao usuário
+    print (f"Prioridade: {tarefa_completa['prioridade']} | Data: {tarefa_completa['data']}") # Resposta ao usuário
 
     salvar_tarefas_txt() # Só lembrando de salvar o arquivo depois de ter adicionado alguma tarefa
 
-def cumprir_ultima_tarefa(): # Função para remover a última tarefa que foi adicionada na lista (Pilha - LIFO)
+def cumprir_ultima_tarefa(): # Função para remover a última tarefa que foi adicionada na lista,no estilo pilha (LIFO)
     if historico:
         ultima = historico.pop()
         tarefas.remove(ultima)
@@ -47,7 +57,7 @@ def cumprir_ultima_tarefa(): # Função para remover a última tarefa que foi ad
     else:
         print ("Ops... Nenhuma tarefa para desfazer. \n") # Resposta ao usuário
 
-def cumprir_primeira_tarefa(): # Função para remover a primeira tarefa que foi adicionada na lista (Fila - FIFO)
+def cumprir_primeira_tarefa(): # Função para remover a primeira tarefa que foi adicionada na lista, no estilo fila (FIFO)
     if fila_execucao:
         feita = fila_execucao.pop(0)
         tarefas.remove(feita)
@@ -56,7 +66,7 @@ def cumprir_primeira_tarefa(): # Função para remover a primeira tarefa que foi
 
         salvar_tarefas_txt() # Só lembrando de salvar o arquivo depois de ter atendido alguma tarefa
     else:
-        print ("Ops... Nenhuma tarefa para atender. \n")
+        print ("Ops... Nenhuma tarefa para atender. \n") # Resposta ao usuário
 
 def mostrar_tarefas(): # Função para mostrar as tarefas disponiveis
     print ()
@@ -64,7 +74,7 @@ def mostrar_tarefas(): # Função para mostrar as tarefas disponiveis
     print ()
 
     for i, t in enumerate(tarefas):
-        print (f"{i + 1}. {t['tarefa']} | Prioridade: {t['prioridade']} | Data: {t['data']}")
+        print (f"{i + 1}. {t['tarefa']} | Prioridade: {t['prioridade']} | Data: {t['data']}") # Resposta ao usuário
 
 def salvar_tarefas_txt(): # Função para salvar as tarefas no arquivo .txt sempre que houver uma mudança na lista de tarefas
     # Abre o arquivo em modo de escrita, especificando o processo de execução e a codificação (aceitando caracteres especiais)
@@ -82,7 +92,7 @@ while True: # Laço de repetição que fica mostrando as opções do menu enquan
     
     # Menu de opções
     print()
-    print ("🗓️   -  Agenda de tarefas interativa:")
+    print ("🗓️   Agenda de tarefas interativa ↴")
     print()
     
     print ("------------------------------------")
